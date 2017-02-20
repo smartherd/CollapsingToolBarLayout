dea7c635da421a355dcf19f53d5f1008e01dc115
Alex Birkett <alex@birkett.co.uk>
2017-02-20 21:04:16 +0100

    Replace NestedScrollView/LinearLayout view with RecyclerView
    Using a RecyclerView is preferable because not all 9 copies of the item_nested_scrollview will be loaded into the view hiercharchy

--- app/src/main/java/com/annie/smartherd/collapsingtoolbarlayout/MainActivity.java
+++ app/src/main/java/com/annie/smartherd/collapsingtoolbarlayout/MainActivity.java
@@ -3,9 +3,16 @@ package com.annie.smartherd.collapsingtoolbarlayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int ITEM_COUNT = 9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +25,31 @@ public class MainActivity extends AppCompatActivity {
         CollapsingToolbarLayout collapsingToolbar =
                 (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar);
         collapsingToolbar.setTitle("Collapsing Toolbar");
+        setupRecyclerView();
+    }
+
+    private void setupRecyclerView() {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item, parent, false);
+                RecyclerView.ViewHolder viewHolder = new RecyclerView.ViewHolder(view){};
+                return viewHolder;
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+                // no data to bind
+            }
+
+            @Override
+            public int getItemCount() {
+                return ITEM_COUNT;
+            }
+        });
     }
 }
